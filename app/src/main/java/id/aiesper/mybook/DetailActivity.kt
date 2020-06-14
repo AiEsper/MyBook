fa159579425c1cac5c2e9031d6e21a1d1cc202c4
AiEsper <AiEsper@users.noreply.github.com>
2020-06-15 02:55:32 +0700

    Update DetailActivity.kt

--- app/src/main/java/id/aiesper/mybook/DetailActivity.kt
+++ app/src/main/java/id/aiesper/mybook/DetailActivity.kt
@@ -2,28 +2,29 @@ package id.aiesper.mybook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
+import android.widget.ScrollView
 import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBar
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
-class detail_book : AppCompatActivity() {
-
+class DetailActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layout)
-        val actionbar = supportActionBar
-        actionbar!!.title = "Detail Buku"
-        actionbar.setDisplayHomeAsUpEnabled(true)
 
         val tvTitle: TextView = findViewById(R.id.book_title)
         val imgSetPhoto: ImageView = findViewById(R.id.img_item_photo)
-        //val tvSetIden: TextView = findViewById(R.id.tv_set_identity)
         val tvAuthor: TextView = findViewById(R.id.book_author)
         val tvDesc: TextView = findViewById(R.id.book_desc )
         val tvType: TextView = findViewById(R.id.book_type)
         val tvGenre: TextView = findViewById(R.id.book_genre)
+        //val toolbarDetail: Toolbar   = findViewById(R.id.detail_toolbar)
+        //val toolbarDetTitle: TextView = findViewById(R.id.toolbar_title)
+        //val detailScrollView: ScrollView = findViewById(R.id.detail_scrollView)
 
         val tTitle  = intent.getStringExtra(EXTRA_TITLE)
         val tImg = intent.getStringExtra(EXTRA_PHOTO)
@@ -31,7 +32,6 @@ class detail_book : AppCompatActivity() {
         val tDesc = intent.getStringExtra(EXTRA_DESC)
         val tType = intent.getStringExtra(EXTRA_TYPE)
         val tGenre = intent.getStringExtra(EXTRA_GENRE)
-
 
         tvTitle.text = tTitle
         Glide.with(this)
@@ -58,4 +58,5 @@ class detail_book : AppCompatActivity() {
         onBackPressed()
         return true
     }
+
 }
